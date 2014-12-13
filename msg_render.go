78a609bd0f307641303ed518a8cfea01f7b97591
Kendell Fabricius <me@kendellfabrici.us>
2014-12-13 15:48:49 -0700

    Changed the default renderer to just renderer.  Added a simple message renderer wrapper.

---
+++ msg_render.go
@@ -0,0 +1,60 @@
+package milo
+
+import (
+	"bytes"
+	"errors"
+	html "html/template"
+	"path/filepath"
+	"text/template"
+)
+
+type MsgRender struct {
+	tplDir   string
+	tplFuncs map[string]interface{}
+}
+
+func NewMsgRender(tplDir string) *MsgRender {
+	m := &MsgRender{tplDir: tplDir}
+	m.tplFuncs = make(map[string]interface{})
+	return m
+}
+
+func (m *MsgRender) Render(data map[string]interface{}, tpls ...string) (string, error) {
+	if len(tpls) < 1 {
+		return "", errors.New("Template identifiers required to render.")
+	}
+	list := make([]string, 0)
+	for _, elem := range tpls {
+		list = append(list, filepath.Join(m.tplDir, elem))
+	}
+
+	if tpl, tplErr := template.New(filepath.Base(tpls[0])).ParseFiles(list...); tplErr != nil {
+		return "", tplErr
+	} else {
+		output := bytes.NewBufferString("")
+		if execErr := tpl.Execute(output, data); execErr != nil {
+			return "", execErr
+		}
+		return output.String(), nil
+	}
+}
+
+func (m *MsgRender) RenderHtml(data map[string]interface{}, tpls ...string) (string, error) {
+	if len(tpls) < 1 {
+		return "", errors.New("Template identifiers required to render.")
+	}
+	list := make([]string, 0)
+	for _, elem := range tpls {
+		list = append(list, filepath.Join(m.tplDir, elem))
+	}
+
+	if tpl, tplErr := html.New(filepath.Base(tpls[0])).ParseFiles(list...); tplErr != nil {
+		return "", tplErr
+	} else {
+		output := bytes.NewBufferString("")
+		if execErr := tpl.Execute(output, data); execErr != nil {
+			return "", execErr
+		}
+		return output.String(), nil
+	}
+}
--- render.go
+++ render.go
@@ -10,23 +10,13 @@ import (
 	"sync"
 )
 
-// An interface of suggested methods for rendering output in Go
-type Renderer interface {
-	RenderTemplates(w http.ResponseWriter, r *http.Request, data map[string]interface{}, tpls ...string)
-	RenderJson(w http.ResponseWriter, r *http.Request, data interface{})
-	RenderError(w http.ResponseWriter, r *http.Request, code int, message string)
-	RenderMessage(w http.ResponseWriter, r *http.Request, message string)
-	RegisterTemplateFunc(key string, fn interface{})
-	Redirect(w http.ResponseWriter, r *http.Request, url string, code int)
-}
-
 // An interface to define a way to get config items always into template rendering
 type Configer interface {
 	GetConfig(key string) interface{}
 }
 
 // Default milo renderer that can cache templates, sets a base template directory.
-type DefaultRenderer struct {
+type Renderer struct {
 	templateCache map[string]*template.Template
 	tplDir        string
 	tplFuncs      map[string]interface{}
@@ -36,8 +26,8 @@ type DefaultRenderer struct {
 }
 
 // Create a new default milo renderer.
-func NewDefaultRenderer(tplDir string, cache bool, configer Configer) Renderer {
-	r := &DefaultRenderer{templateCache: make(map[string]*template.Template), tplDir: tplDir, tplFuncs: make(map[string]interface{}), cacheTpls: cache, configer: configer}
+func NewRenderer(tplDir string, cache bool, configer Configer) *Renderer {
+	r := &Renderer{templateCache: make(map[string]*template.Template), tplDir: tplDir, tplFuncs: make(map[string]interface{}), cacheTpls: cache, configer: configer}
 	r.tplFuncs["host"] = Host
 	r.tplFuncs["marshal"] = Marshal
 	r.tplFuncs["partial"] = r.Partial
@@ -45,7 +35,7 @@ func NewDefaultRenderer(tplDir string, cache bool, configer Configer) Renderer {
 }
 
 // Takes care of rendering templates from file.
-func (mr *DefaultRenderer) RenderTemplates(w http.ResponseWriter, r *http.Request, data map[string]interface{}, tpls ...string) {
+func (mr *Renderer) RenderTemplates(w http.ResponseWriter, r *http.Request, data map[string]interface{}, tpls ...string) {
 	if len(tpls) < 1 {
 		w.WriteHeader(500)
 		w.Write([]byte("Error: Template required!"))
@@ -83,7 +73,7 @@ func (mr *DefaultRenderer) RenderTemplates(w http.ResponseWriter, r *http.Reques
 // Unexported method to help handle template parsing.  If the cache template bool is set on the config
 // struct this method with look in the cache & load the cache upon subsequent encounters.
 // This should lower disk access penalties useful for production instances.
-func (mr *DefaultRenderer) acquireTemplate(key string, tpls ...string) (*template.Template, error) {
+func (mr *Renderer) acquireTemplate(key string, tpls ...string) (*template.Template, error) {
 	var tpl *template.Template
 	var loadErr error
 	var ok bool
@@ -111,7 +101,7 @@ func (mr *DefaultRenderer) acquireTemplate(key string, tpls ...string) (*templat
 }
 
 // Render json output
-func (mr *DefaultRenderer) RenderJson(w http.ResponseWriter, r *http.Request, data interface{}) {
+func (mr *Renderer) RenderJson(w http.ResponseWriter, r *http.Request, data interface{}) {
 	if data, err := json.Marshal(data); err != nil {
 		w.WriteHeader(500)
 		w.Write([]byte(err.Error()))
@@ -122,28 +112,28 @@ func (mr *DefaultRenderer) RenderJson(w http.ResponseWriter, r *http.Request, da
 	}
 }
 
-func (mr *DefaultRenderer) RenderError(w http.ResponseWriter, r *http.Request, code int, message string) {
+func (mr *Renderer) RenderError(w http.ResponseWriter, r *http.Request, code int, message string) {
 	w.WriteHeader(code)
 	w.Write([]byte(message))
 }
 
-func (mr *DefaultRenderer) RenderMessage(w http.ResponseWriter, r *http.Request, message string) {
+func (mr *Renderer) RenderMessage(w http.ResponseWriter, r *http.Request, message string) {
 	w.WriteHeader(200)
 	w.Write([]byte(message))
 }
 
 // Register a template function with the MiloRenderer
-func (mr *DefaultRenderer) RegisterTemplateFunc(key string, fn interface{}) {
+func (mr *Renderer) RegisterTemplateFunc(key string, fn interface{}) {
 	mr.tplFuncs[key] = fn
 }
 
 // Setup an http redirect on the request.
-func (mr *DefaultRenderer) Redirect(w http.ResponseWriter, r *http.Request, url string, code int) {
+func (mr *Renderer) Redirect(w http.ResponseWriter, r *http.Request, url string, code int) {
 	http.Redirect(w, r, url, code)
 }
 
 // A template function which can include a partial template.
-func (mr *DefaultRenderer) Partial(name string, payload interface{}) (template.HTML, error) {
+func (mr *Renderer) Partial(name string, payload interface{}) (template.HTML, error) {
 	var buff bytes.Buffer
 	path := filepath.Join(mr.tplDir, "partials", name)
 
